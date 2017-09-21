77e06ba8cc158bd342037403bc681333f2dc2b14
alanionita <a.g.ionita@gmail.com>
2017-09-21 21:08:11 +0100

    [react][comp] added styling to the ArticleSingle comp

--- src/components/presentational/ArticleSingle.js
+++ src/components/presentational/ArticleSingle.js
@@ -6,22 +6,28 @@ import CommentCard from './CommentCard';
 
 const ArticleSingle = ({ article, comments }) => {
   return (
-    <div>
-      <h3>Title: {article.title}</h3>
-      <p>Body: {article.body}</p>
-      <p>Votes: {article.votes}</p>
-      <p>Topic: {article.belongs_to}</p>
-      <p>Author: {article.created_by}</p>
-      <h1>Comments:</h1>
-      {comments.map(comment => (
-        <CommentCard
-          body={comment.body}
-          votes={comment.votes}
-          created_by={comment.created_by}
-          created_at={comment.created_at}
-          key={comment._id}
-        />
-      ))}
+    <div className="container is-fluid">
+      <section className="box">
+        <h1 className="title is-3">
+          {article.title}{' '}
+          <span className="tag is-light">{article.belongs_to}</span>
+        </h1>
+        <p className="subtitle is-6">submited by {article.created_by}</p>
+        <p className="content">{article.body}</p>
+        <p>Votes: {article.votes}</p>
+      </section>
+      <section className="container">
+        <h2 className="subtitle is-4">all {comments.length} comments</h2>
+        {comments.map(comment => (
+          <CommentCard
+            body={comment.body}
+            votes={comment.votes}
+            created_by={comment.created_by}
+            created_at={comment.created_at}
+            key={comment._id}
+          />
+        ))}
+      </section>
     </div>
   );
 };

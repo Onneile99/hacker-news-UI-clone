6f15d2a652f4638399e7aeacd0edca274938da52
alanionita <a.g.ionita@gmail.com>
2017-10-08 14:58:46 +0100

    [react][comp] updated the html on ArticleVotingSegment

--- src/components/presentational/ArticleVotingSegment.js
+++ src/components/presentational/ArticleVotingSegment.js
@@ -5,11 +5,21 @@ const ArticleVotingSegment = ({ votes, article_id, alterArticleVotes }) => (
   <div className="tile is-ancestor">
     <div className="tile is-1 is-vertical">
       <div className="tile">
-        <i onClick={() => alterArticleVotes(article_id, 'up')} className="fa fa-chevron-up" aria-hidden="true" />
+        <a
+          onClick={() => alterArticleVotes(article_id, 'up')}
+          className="fa fa-chevron-up"
+          aria-hidden="true"
+          alt="upvote"
+        />
       </div>
       <div className="tile">{votes}</div>
       <div className="tile">
-        <i onClick={() => alterArticleVotes(article_id, 'down')} className="fa fa-chevron-down" aria-hidden="true" />
+        <a
+          onClick={() => alterArticleVotes(article_id, 'down')}
+          className="fa fa-chevron-down"
+          aria-hidden="true"
+          alt="downvote"
+        />
       </div>
     </div>
   </div>
